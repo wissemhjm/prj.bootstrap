630fb69faa621dd7e8f3b08ffc5b2d24eea5109b
wissemhjm <129502701+wissemhjm@users.noreply.github.com>
2023-05-09 08:54:56 +0100

    your message

---
+++ main.js
@@ -0,0 +1,27 @@
+let posts = [];
+
+function createPost() {
+  let title = document.getElementById("title").value;
+  let subject = document.getElementById("subject").value;
+  let imageUrl = document.getElementById("image-url").value;
+  
+  let post = { title: title, subject: subject, imageUrl: imageUrl };
+  posts.push(post);
+  displayPosts();
+}
+
+function displayPosts() {
+  let Maker = "";
+  
+  for (let i = 0; i < posts.length; i++) {
+    let post = posts[i];
+    
+    Maker += "<div class='post'>";
+    Maker += "<h2>" + post.title + "</h2>";
+    Maker += "<p>" + post.subject + "</p>";
+    Maker += "<img src='" + post.imageUrl + "'>";
+    Maker += "</div>";
+  }
+  
+  document.getElementById("posts").innerHTML = Maker;
+}
